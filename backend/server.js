d824708616ee2a7c2e6044db26e56fddec261544
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-25 22:24:39 +0000

    update

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.get("/products", (req, res) => {});
 
-console.log(process.env.MONGO_uri)
-
 app.listen(5000, () => {
+  connectDB();
   console.log("Server started at http://localhost:5000");
 });
