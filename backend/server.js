0dba631cd8860286661f8197c1fb53a8adaf5636
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-25 22:54:22 +0000

    Testing Express and Mongo server

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,22 @@ dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.post("/products", async(req, res) => {
+    const product = req.body;
+    if(!product.name){
+        return res.status(400).json({success: false, message: "Please fill the field"})
+    }
+
+    const newProduct = new Product(product)
+
+    try{
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct})
+    }catch{
+        console.error(`Error in Creating Data ${error.message}`)
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+});
 
 app.listen(5000, () => {
   connectDB();
