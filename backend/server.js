602a892cea05166fd3656ffb30dd1132c6c7dff3
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-11-03 11:34:06 +0000

    Created CRUD endpoints - next including security

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,38 @@ const app = express();
 
 app.use(express.json()); //For accepting json in data body
 
+// Get all products
+app.get("/api/products", async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+    res.status(200).json({ success: true, data: allProducts });
+  } catch (error) {
+    console.error(`Error in Fetching Products ${error.message}`);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+// Update product
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+
+  if (!updateData) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  try {
+    const updateProduct = await Product.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+    res.status(200).json({ succes: true, data: updateProduct });
+  } catch (error) {
+    console.log(`Error in Updating Products ${error.message}`);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
 
 // Create a product
 app.post("/api/products", async (req, res) => {
@@ -30,24 +62,21 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
-
 // Delete a specific product
 app.delete("/api/products/:id", async (req, res) => {
   const { id } = req.params;
 
-  try{
+  try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted" });
-  } catch(error){
+  } catch (error) {
     res.status(404).json({ success: false, message: "Product not found" });
   }
 });
 
-
 app.get("/", (req, res) => {
   res.send("The server is running.");
 });
-
 
 app.listen(5000, () => {
   connectDB();
