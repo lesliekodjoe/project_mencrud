72d262f0b3a6f375fd79243bca65c3e812a1fe47
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-11-03 17:38:41 +0000

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
+import CreatePage from "./pages/CreatePage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/CreatePage.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+  return (
+    <div>
+      <div className="w-full flex flex-col items-center justify-center">
+        <div className="w-[600px]">
+          <h1 className="text-center text-5xl font-medium">Add a Product</h1>
+          <div className="py-20">
+            <form action="" className="flex flex-col gap-y-8">
+              <input
+                type="text"
+                name="name"
+                className="bg-gray-100 py-2 px-6 rounded-sm"
+                placeholder="Product Name"
+                value={newProduct.name}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, name: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                name="price"
+                placeholder="Product Price"
+                className="bg-gray-100 py-2 px-6 rounded-sm"
+                value={newProduct.name}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, price: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                name="image"
+                placeholder="Image URL"
+                className="bg-gray-100 py-2 px-6 rounded-sm"
+                value={newProduct.name}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, image: e.target.value })
+                }
+              />
+              <button className="bg-gray-500 py-3 text-white rounded-sm mt-4">Add Product</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
