1346935606aab3413b8970237b59cdfd3f616125
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-11-04 09:31:13 +0000

    Update

--- frontend/src/sections/HeroSection.tsx
+++ frontend/src/sections/HeroSection.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div className='container mx-auto px-12 w-full'>
+      <div className='px-20 w-full flex flex-col items-center justify-center text-center'>
+        <h4 className='text-5xl font-medium underline underline-offset-8'>Product Listing</h4>
+        <div className='mt-20'>
+          <p>No Product found</p>
+          <Link to={"/create"} className='text-blue-500 hover:underline'>Create a Product</Link>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
